5340f13f781be54df7e7b6ac0b91a2d6b13906f8
mpukabaofficial <99944714+mpukabaofficial@users.noreply.github.com>
2024-07-30 12:38:52 -0400

    Bult a styled link component

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -2,7 +2,7 @@ import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import React from "react";
+import ReactMarkdown from "react-markdown";
 
 interface Props {
   params: {
@@ -23,13 +23,12 @@ const DetailPage = async ({ params }: Props) => {
   return (
     <div>
       <Heading>{issue.title}</Heading>
-      <IssueStatusBadge status={issue.status} />
-      <Flex gap="4">
+      <Flex gap="4" className="mb-4">
+        <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
-        <Text>{issue.description}</Text>
       </Flex>
-      <Card>
-        <Text>{issue.description}</Text>
+      <Card className="prose mt-4">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </div>
   );
