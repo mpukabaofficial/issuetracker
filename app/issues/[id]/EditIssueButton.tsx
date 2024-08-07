e122c19da1bef7a30f3eb427441560f6418ed0f6
mpukabaofficial <99944714+mpukabaofficial@users.noreply.github.com>
2024-08-07 15:15:41 -0400

    Refactor: apply the single responsibility principle

---
+++ app/issues/[id]/EditIssueButton.tsx
@@ -0,0 +1,18 @@
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
+
+interface Props {
+  issueId: number;
+}
+
+const EditIssueButton = ({ issueId }: Props) => {
+  return (
+    <Button className="w-fit">
+      <Pencil2Icon /> <Link href={`/issues/${issueId}/edit`}>Edit Issue</Link>
+    </Button>
+  );
+};
+
+export default EditIssueButton;
