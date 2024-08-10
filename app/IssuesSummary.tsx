18a1ec69d907d71a07162a0de7030c76d3ba9036
mpukabaofficial <99944714+mpukabaofficial@users.noreply.github.com>
2024-08-10 15:13:26 -0400

    Built the issue chart component

---
+++ app/IssuesSummary.tsx
@@ -0,0 +1,39 @@
+import { Status } from "@prisma/client";
+import { Card, Flex, Text } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
+
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+
+const IssuesSummary = ({ closed, inProgress, open }: Props) => {
+  const statuses: { label: string; value: number; status: Status }[] = [
+    { label: "Open Issues", value: open, status: "OPEN" },
+    { label: "In Progress Issues", value: inProgress, status: "IN_PROGRESS" },
+    { label: "Closed Issues", value: closed, status: "CLOSED" },
+  ];
+  return (
+    <Flex gap="4">
+      {statuses.map(({ label, status, value }) => (
+        <Card key={label}>
+          <Flex direction="column" gap="1">
+            <Link
+              className="text-sm font-medium"
+              href={`/issues/list?status=${status}`}
+            >
+              {label}
+            </Link>
+            <Text size="5" className="font-bold">
+              {value}
+            </Text>
+          </Flex>
+        </Card>
+      ))}
+    </Flex>
+  );
+};
+
+export default IssuesSummary;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
-import LatestIssues from "./LatestIssues";
+import prisma from "@/prisma/client";
+import IssuesSummary from "./IssuesSummary";
+import IssueChart from "./IssueChart";
 
-export default function Home() {
-  return <LatestIssues />;
+export default async function Home() {
+  const openIssues = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgressIssues = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closedIssues = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+  return (
+    <IssueChart
+      open={openIssues}
+      inProgress={inProgressIssues}
+      closed={closedIssues}
+    />
+  );
 }
